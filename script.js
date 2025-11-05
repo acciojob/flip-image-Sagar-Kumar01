c2c30c808d55ec100d226be8308582324e5da7db
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-11-05 20:00:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // write js code if required
+let flip = document.getElementById("flip");
+
+flip.addEventListener("mouseenter", ()=>{
+	flip.setAttribute("src","https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg");
+})
+
+flip.addEventListener("mouseout", ()=>{
+	flip.setAttribute("src","https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg");
+})
